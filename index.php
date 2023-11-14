6f2c2f5ece2f250e87e2a9579c70e636d28b82c8
IceCreamPink <henradwi1@gmail.com>
2023-11-14 11:02:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Resto Nyaa</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Hot Promo</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Pilihan Box</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="menu">
+        <h2>Menu Kami</h2>
+        <div class="menu-item">
+            <img src="img/soto.jpg">
+            <h3>Natsu</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, ad?</p>
+            <p class="harga">Rp.15.000</p>
+            <button class="btn">Beli</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/soto.jpg">
+            <h3>Cimo</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, ad?</p>
+            <p class="harga">Rp.15.000</p>
+            <button class="btn">Beli</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/soto.jpg">
+            <h3>Ngai</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, ad?</p>
+            <p class="harga">Rp.15.000</p>
+            <button class="btn">Beli</button>
+        </div>
+    </section>
+    <section class="contact">
+        <h2>Hubungi Kami</h2>
+        <p>Jika ada pertanyaan, saran, dan keritik hubungi kami di</p>
+        <p>Wa: 085604671200</p>
+        <p>Gmail : IceCreamPink@gmail.com</p>
+    </section>
+    <footer>
+        <p>&copy; 2023 Restoran Enak .Hak Cipta di Lindungi</p>
+    </footer>
+</body>
+
+</html>
